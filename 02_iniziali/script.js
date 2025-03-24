d36f2deeb5a7d101f85080bb06815812427ddc2b
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-24 16:50:47 +0100

    [UPD] insert variables in for cycle for extract first letter

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,18 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-function firstLetter(string) {
+function letters(string) {
     let newArray = [];
 
     for(i=0; i<string.lenght; i++){
+        let words = string.split('');
 
+        const firstWords = words.charAT(0).toUpperCase();
+
+        const restofWords = words.substring(1);
+
+        const newArray = firstWords.push(newArray); 
     }
 
+    return newArray;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-let result = firstLetter(names);
+let result = letters(names);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
